d61dbd54756b5f2fdf0389564c1fb1652f9d052b
remly <remly@qq.com>
2019-12-03 22:39:04 +0800

    token_protect

--- app/api/auth.py
+++ app/api/auth.py
@@ -4,8 +4,10 @@ from app.models import User
 from app.api.errors import error_response
 
 #这是一个basic认证的入口，在其他地方导入basic_auth
+# basic_auth用于登录认证用户
 basic_auth=HTTPBasicAuth()
 
+# 登录后的认证依靠token_token
 token_auth = HTTPTokenAuth()
 
 @basic_auth.verify_password
