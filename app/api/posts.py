19f04d2a528a6f2f91bb7e2f50e4126e6b04bbc1
remly <remly@qq.com>
2019-12-13 15:52:45 +0800

    getpost

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,7 +1,7 @@
 from app import db
 from app.models import Post
 from app.api import bp
-from app.api.errors import bad_request
+from app.api.errors import bad_request,error_response
 from app.api.auth import token_auth
 
 from flask import request,jsonify,g
@@ -24,8 +24,10 @@ def create_post():
     post.from_dict(data)
     #（？）g.current_user是当前登录用户的一个User对象，可以把对象挂在post实例的属性上的吗？
     #       ....换句话提问就是数据库能储存对象了？
+    #       ....既然没有作为字段存在数据库中，为什么在修改文章（update_post）中仍然可以访问post.author
     #   1.初步推测挂在post上的新增属性，但是类属性里面并没有定义，因此不会提交到数据库
     #       ....而是作为一个普通属性来使用
+    #   2.有点明白了，请仔细阅读sqlalchemy文档，backref的相关内容
     #（？）为什么会出现错误？g对象没有current_user？
     #   g对象在auth.py中出现，换句话说通过验证后才会有current_user
     #   所以，你tm倒是把验证的装饰器给加上去啊！
@@ -41,18 +43,27 @@ def get_posts():
     page = request.args.get('page',1,type=int)
     per_page=request.args.get('per_page',10,type=int)
     post = Post()
-    response = post.to_collection_dict(Post.query,page,per_page,'/api.get_posts')
+    #这里返回的集合按照时间排序
+    #参照flask-sqlalchemy文档：When you access it you will get back a new query object over all records.
+    #Post.query会返回Post表中所有记录
+    response = post.to_collection_dict(Post.query.order_by(Post.timestamp.desc()),page,per_page,'/api.get_posts')
     return jsonify(response)
 
 @bp.route('/posts/<int:id>',methods=['GET'])
 def get_post(id):
-    '''返回单个post'''
-    return jsonify(Post.query.get_or_404(id).to_dict())
+    '''返回一篇文章'''
+    post = Post.query.get_or_404(id)
+    post.views += 1
+    db.session.add(post)
+    db.session.commit()
+    return jsonify(post.to_dict())
 
 @bp.route('/posts/<int:id>',methods=['PUT'])
 def update_post(id):
     '''修改post'''
     post = Post.query.get_or_404(id)
+    if g.current_user != post.author:
+        return error_response(403)
     data = request.get_json()
     if not data:
         return bad_request("Json expected!")
@@ -67,3 +78,12 @@ def update_post(id):
     post.from_dict(data)
     db.session.commit()
     return jsonify(post.to_dict())
+
+@bp.route('/posts/<int:id>',methods=['DELETE'])
+def delete_post(id):
+    post = Post.query.get_or_404(id)
+    if g.current_user != post.author:
+        return error_response(403)
+    db.session.delete(post)
+    db.session.commit()
+    return '',204
