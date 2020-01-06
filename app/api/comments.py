8e89b24e1abf5113a757bfa78ec0bd201e52029d
remly <remly@qq.com>
2020-01-06 15:56:18 +0800

    comment

--- app/api/comments.py
+++ app/api/comments.py
@@ -30,6 +30,7 @@ def create_comment():
 @bp.route('/comments',methods=['GET'])
 @token_auth.login_required
 def get_comments():
+    '''Get All Comments'''
     page = request.args.get('page',1,type=int)
     per_page=min(request.args.get(
         'per_page',current_app.config['COMMENT_PER_PAGE'],type=int
